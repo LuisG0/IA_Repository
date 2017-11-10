7351e7ebce081bf53b3c784e29f4b1a445fed350
Lui_G07 <luisgl.spain@gmail.com>
2017-11-10 22:07:45 +0100

    Board and Pieces

---
+++ IA_Proyect/Assets/Scripts/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public float speed = 2;
+    public float translationSpeed = 1;
+    public float translationRange = 10;
+    Vector3 velocity;
+    float translation = 0;
+
+    // Use this for initialization
+    void Start () {
+    
+    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        translation += translationSpeed * Time.deltaTime;
+        velocity = new Vector3(speed, Mathf.Cos(translation) * translationRange * translationSpeed, 0);
+
+
+        transform.position += velocity * Time.deltaTime;		
+
+	}
+}
