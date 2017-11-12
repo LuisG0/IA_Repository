8dbed046fd8513d6a29f43097223b89317906647
Lui_G07 <luisgl.spain@gmail.com>
2017-11-13 00:09:31 +0100

    Dice 1

---
+++ IA_Proyect/Assets/DiceRoller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour {
+
+	void Start () {
+        DiceValues = new int[4];
+	}
+	
+	void Update () {
+		
+	}
+    public int[] DiceValues;
+    public int DiceTotal;
+
+    public void RollTheDice()
+    {
+        //in Ur, you roll four dice ( classically Tetahedron), which
+        //have half their faces have a value of "1" and half have a value of zero.
+
+        //You COULD roll actual physics enabled dice.
+
+        //We are going to use random number generation instead.
+
+        DiceTotal = 0;
+        for (int i = 0; i < DiceValues.Length; i++)
+        {
+            DiceValues[i] = Random.Range(0, 2);
+            DiceTotal += DiceValues[i];
+        }
+
+        Debug.Log("Rolled " + DiceValues + " (" + DiceTotal + ")");
+    }
+}
